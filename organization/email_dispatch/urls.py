1c7def8207322deb217f80e4a6f50d04d8936c9c
Михайлов Михаил <mihdevel@gmail.com>
2018-08-17 17:40:30 +0300

    Created functions unsubscribe

--- organization/email_dispatch/urls.py
+++ organization/email_dispatch/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'email_dispatch'
 urlpatterns = [
+    path('unsubscribe_form', views.unsubscribe_form, name='unsubscribe_form'),
     path('unsubscribe_send', views.unsubscribe_send, name='unsubscribe_send'),
-    path('unsubscribe_get', views.unsubscribe_get, name='unsubscribe_get'),
+    path('unsubscribe', views.unsubscribe, name='unsubscribe'),
 ]
--- organization/email_dispatch/views.py
+++ organization/email_dispatch/views.py
@@ -1,9 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
+from .models import Email
+import hashlib
+import smtplib
+
+
+
+def unsubscribe_form(request):
+  return render(request, 'email_dispatch/unsubscribe_form.html')
+
+
+def get_hash_with_email(email):
+  return hashlib.sha256(bytes(email + str(date.today()), encoding='utf-8')).hexdigest()
 
 
 def unsubscribe_send(request):
-  pass
+  email = request.POST['email']
+  
+  if not Email.objects.filter(email=email):
+    return HttpResponse('{}, к счастью, а может и нет, но Вы не подписаны на нашу рассылку!'.format(email))
 
-def unsubscribe_get(request):
-  pass
+  hash = get_hash_with_email(email)
+  url_unsubscribe = 'http://127.0.0.1:8000/unsubscribe?email={}&hash={}'.format(email,hash)
+  
+  text_email = 'Для того, чтобы отписаться от рассылки перейдите по ссылке\n' \
+       '<a href"{}">{}</a>'.format(url_unsubscribe, url_unsubscribe)
+  
+  # С отправкой письма на email я еще не разобрался,
+  # но она должна осуществляться здесь!
+
+  text_return = '{}, мы отправили на вашу эл. почту письмо для подтверждения отписки от рассылки! ' \
+                'Пожалуйста, проверьте её!'
+  return HttpResponse(text_return.format(email))
+
+
+def unsubscribe(request):
+  email = request.GET['email']
+  hash_input =  request.GET['hash']
+  my_hash = get_hash_with_email(email)
+  
+  if hash_input != my_hash:
+    return HttpResponse('Ой ошибочка, нами получен не правильный хеш, попробуйте заново отписаться!'.format(email))
+  
+  email_checked = Email.objects.filter(email=email)
+
+  if not email_checked:
+    return HttpResponse('К счастью, а может и нет, но адрес эл. почты "{}" не подписан на нашу рассылку!'.format(email))
+  
+  email_checked.delete()
+  return HttpResponse('{}, спасибо, Вы успешно отписались от нашей рассылки!'.format(email))
