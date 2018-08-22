6716469870084bd71632aac8c1ea156ee9b1bab6
Михайлов Михаил <mihdevel@gmail.com>
2018-08-22 13:29:44 +0300

    Edited email_dispatch/models.py

--- organization/email_dispatch/admin.py
+++ organization/email_dispatch/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Email, Dispatch, Group, Report
+from .models import Email, Dispatch, Group, Template, Report
 
 admin.site.register(Email)
 admin.site.register(Group)
 admin.site.register(Dispatch)
+admin.site.register(Template)
 admin.site.register(Report)
--- organization/email_dispatch/models.py
+++ organization/email_dispatch/models.py
@@ -18,23 +18,20 @@ class Group(models.Model):
 
 class Dispatch(models.Model):
   template_id = models.ForeignKey('Template', on_delete=models.CASCADE)
-  group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
   date = models.DateTimeField()
-  text = models.TextField()
+  
+  def __str__(self):
+    return str(self.template_id)
 
 
 class Report(models.Model):
   dispatch_id = models.ForeignKey(Dispatch, on_delete=models.CASCADE)
   status = models.BooleanField()
-  
-  def __str__(self):
-    return self.status
 
 
 class Template(models.Model):
   name = models.CharField(max_length=100)
   group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
-  date = models.DateTimeField()
   text = models.TextField()
   
   def __str__(self):
